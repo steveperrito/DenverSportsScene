ea4397e6b0cb33b027d6dc0a6b95a4684743809c
Steve Perry <steveperry@Steves-MacBook-Pro.local>
2014-06-24 17:47:57 -0600

    updated code to write body

--- js/body.js
+++ js/body.js
@@ -1,23 +1,28 @@
-var nfl = {
-    homeTeam: "Denver Broncos",
-    awayTeam: "Seattle Seahawks",
-    time: new Date(2014, 9, 21, 14, 25)
-};
+games = [{ sport: "nfl", homeTeam: "Denver Broncos", awayTeam: "Seattle Seahawks", time: new Date(2014, 9, 21, 14, 25) },
 
-var mlb = {
-    homeTeam: "Colorado Rockies",
-    awayTeam: "Arizona Diamondbacks",
-    time: new Date(2014, 9, 21, 14, 10)
-};
+{ sport: "mlb", homeTeam: "Colorado Rockies", awayTeam: "Arizona Diamondbacks", time: new Date(2014, 9, 21, 14, 10) },
 
-var todaysGames = [nfl, mlb];
+{ sport: "nba", homeTeam: "Denver Nuggets", awayTeam: "Golden State Warriors", time: new Date(2014, 9, 21, 14, 10) }];
 
-function writeBody () {
-    for (i=0; i<todaysGames.length; i++) {
+
+
+function writeBody(gameArr) {
+
+    for (i=0; i<gameArr.length; i++) {
+
         var gameList = document.createElement('div');
         document.getElementById('GameList').appendChild(gameList);
+
+        var divOrder = document.getElementById('GameList').childNodes;
+
+        if (divOrder[i] == divOrder[0]){
+            gameList.setAttribute("id", "GameXfirst");
+        } else {
         gameList.setAttribute("id", "GameX");
+        }
+
     }
 };
 
-writeBody();
+
+writeBody(games);
