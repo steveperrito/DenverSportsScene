270d7f2e256ac6554740c038945999b0f0ceb60f
Steve Perry <steveperry@Steves-MacBook-Pro.local>
2014-06-28 22:07:28 -0600

    got content to show up in page on Chrome

--- js/body.js
+++ js/body.js
@@ -12,10 +12,13 @@ function writeBody(gameArr) {
 
     for (i=0; i<gameArr.length; i++) {
 
-        var innerContent = ["sport", "versus", "date"];
-        var content = document.createElement('div');
-
+        var innerContent = [
+            {sportAtt: "sport", sport: new Text(games[i].sport.toUpperCase())},
+            {sportAtt: "versus", aTeamText: new Text(games[i].awayTeam), hTeamText: new Text(games[i].homeTeam), vsText: new Text("-Vs-")},
+            {sportAtt: "date", date: new Text(games[i].time)}
+        ];
         var gameList = document.createElement('div');
+
         document.getElementById('GameList').appendChild(gameList);
 
         var divOrder = document.getElementById('GameList').childNodes;
@@ -27,12 +30,31 @@ function writeBody(gameArr) {
         }
 
         innerContent.forEach(function writeInnerContent(x){
+
+            var lineBreak1 = document.createElement('br');
+            var lineBreak2 = document.createElement('br');
+            var content = document.createElement('div');
             var newDiv = document.createElement('div');
+
             gameList.appendChild(newDiv);
-            newDiv.setAttribute("id", x);
+            newDiv.setAttribute("class", x.sportAtt);
             newDiv.appendChild(content);
             content.setAttribute("class","content");
+
+            if (x == innerContent[0]) {
+                content.appendChild(x.sport);
+            } else if (x == innerContent[1]) {
+                content.appendChild(x.aTeamText);
+                content.appendChild(lineBreak1);
+                content.appendChild(x.vsText);
+                content.appendChild(lineBreak2);
+                content.appendChild(x.hTeamText);
+            } else {
+                content.appendChild(x.date);
+            }
+
         });
+
 
     }
 };
